b66f304da3efe62c382889d8875740f7f06bf726
Jan Bojko <honzabojko@seznam.cz>
2024-03-04 10:12:25 +0100

    Vercel deploy try.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				hostname: "localhost",
-				pathname: "**",
-				port: "3000",
-				protocol: "http",
-			},
-			{
-				hostname: "digishark.up.railway.app",
-				protocol: "https",
-			},
-		],
-	},
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+                pathname: "**",
+                port: "3000",
+                protocol: "http",
+            },
+            {
+                hostname: "digishark.up.railway.app",
+                protocol: "https",
+            },
+            {
+                hostname: "digishark.vercel.app",
+                protocol: "https",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
